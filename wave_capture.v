6ce70ce8ba0c5521db4d9755d761c7ef98c5ee3c
marcoandonosie <147224708+marcoandonosie@users.noreply.github.com>
2023-12-13 13:18:02 -0800

    Update wave_capture.v

--- wave_capture.v
+++ wave_capture.v
@@ -1,7 +1,6 @@
 `define ARMED 2'b00
 `define ACTIVE 2'b01
 `define WAIT 2'b10
-`define WIDTH 2
 
 module wave_capture (
     input clk,
@@ -15,98 +14,75 @@ module wave_capture (
     output wire [7:0] write_sample,
     output wire read_index
 );
-    
-    // flip flop to track states
-    wire [1:0] next_state;
-    wire [1:0] cur_state;
-    reg [1:0] next_state_1;
-    dffr #(`WIDTH) dff1 (.clk(clk), .r(reset), .d(next_state), .q(cur_state));
-    
-    // flip flop to track counter (address into RAM)
-    wire [7:0] next_address;
-    wire [7:0] cur_address;
-    reg [7:0] next_address_1;
-    dffr #(.WIDTH(8)) dff2 (.clk(clk), .r(reset), .d(next_address), .q(cur_address)); // width of 8 is correct???
-    
-    // flip flop to track prev and cur sample
-    wire [15:0] input_sample;
-    wire [15:0] prev_sample;
-    dffr #(.WIDTH(16)) dff3 (.clk(clk), .r(reset), .d(input_sample), .q(prev_sample));
-    
-    // temp variable to update read_index
-//    reg update_read_index;
 
-    // flip flop to track read_index value
-    wire next_index;
-    wire cur_index;
-    dffre #(.WIDTH(1)) dff4 (.clk(clk), .r(reset), .d(next_index), .q(cur_index), .en(wave_display_idle == 1'b1 && cur_state == `WAIT));
-    
-    always @(*) begin
-        case(cur_state)
-            `ARMED: begin
-                // signed samples -- 1 indicates a neg number and 0 indicates positive
-                // NEED TO FIX BECAUSE IS THIS CORRECT ON WHEN TO DET3ECT SAMPLE GOING UP??????
-                if (prev_sample[15] == 1'b1 && input_sample[15] == 1'b0) begin // prev_sample == 16'd0 && input_sample[15] == 1'b0) 
-                    next_state_1 = `ACTIVE;
-                    next_address_1 = 8'd0; // used to be cur_address
-                    //next_index_1 = cur_index;
-                    //update_read_index = read_index;
-                end
-                else begin
-                    next_state_1 = `ARMED;
-                    next_address_1 = 8'd0; // used to be cur_address
-                    //next_index_1 = cur_index;
-                    //update_read_index = read_index;
-                end
-            end
-            `ACTIVE: begin
-                if (new_sample_ready == 1'b1) begin
-                    // update counter (will update in one cycle for the next time we get a sample)
-                    // not a problem because we start at zero indexing so no need to wait to send to RAM
-                    if (cur_address == 8'd255) begin //255
-                        next_address_1 = cur_address; // used to be 8'd0
-                        next_state_1 = `WAIT;
-                        //next_index_1 = cur_index;
-                        //update_read_index = read_index;
-                    end
-                    else begin
-                        next_address_1 = cur_address + 8'd1;  
-                        next_state_1 = `ACTIVE;
-                        //next_index_1 = cur_index;
-                        //update_read_index = read_index;
-                    end
-                end
-                else begin
-                    next_address_1 = cur_address;
-                    next_state_1 = `ACTIVE;
-                    //next_index_1 = cur_index;
-                    //update_read_index = read_index;
-                end
-            end
-            `WAIT: begin
-                if (wave_display_idle == 1'b1) begin
-                    //update_read_index = ~read_index;
-                    //next_index_1 = ~cur_index;
-                    next_state_1 = `ARMED;
-                    next_address_1 = cur_address;
-                end
-                else begin
-                    //update_read_index = read_index;
-                   // next_index_1 = cur_index;
-                    next_state_1 = `WAIT;
-                    next_address_1 = cur_address;
-                end
-            end
-        endcase
-    end
-    assign next_state = reset ? `ARMED : next_state_1;
-    assign next_address = reset ? 8'd0 : next_address_1; // width of 8 is correct?
-    assign input_sample = new_sample_in; // need reset? reset ? 16'd0 : 
-    assign next_index = ~read_index; //reset ? 1'b1 : next_index_1; // next_index = ~read_index with an enable
-    
-    // assign values to outputs
-    assign read_index = cur_index;
-    assign write_address = {~read_index, cur_address};
-    assign write_sample = ~new_sample_in[15:8] + 8'd128; // was 128 // cur_state == `ACTIVE ? 
-    assign write_enable = cur_state == `ACTIVE ? 1'b1 : 1'b0;
-endmodule
+	//wires used in the flip flop to keep track of the count and sample
+	wire [7:0] next_counter, counter, write_sample_temp;
+
+	//wires used to keep track of when zero crossing is positive (zero_crossing), sample is negative (is_negative), and when we have counted to 255 (count_done)
+	wire zero_crossing, is_negative, last_is_negative, count_done;
+
+	//wires used in the flip flop to keep track of what state in the FSM we are in
+	wire [1:0] state, next_state;
+
+	//register that allows us to temporarily assign the value of next_state
+	reg [1:0] next_state_temp;
+
+	//register that allows us to temporarily hold and invert the value of read_index
+	wire read_index_temp;
+
+	//flip flop that modifies the address when in the ARMED state
+	dffre #(8) count_flip_flop(.clk(clk), .r(reset | state == `ARMED), .en(state == `ACTIVE), .d(next_counter), .q(counter));
+
+	//flip flop that changes the states 
+	dffr #(2) state_flip_flop(.clk(clk), .r(reset), .d(next_state), .q(state));
+
+	//flip flop that determines positive zero crossing or a negative sample
+	dffre #(1) zero_cross_flip_flop(.clk(clk), .r(reset), .en(~new_sample_ready), .d(is_negative), .q(last_is_negative));
+
+	//flip flop that changes read_index
+	dffre #(1) read_index_flip_flop(.clk(clk), .r(reset), .en(wave_display_idle && state == `WAIT), .d(read_index_temp), .q(read_index));
+
+//change the states based on various signals
+always @ (*) begin
+	case(state)
+		`ARMED: //when in the ARMED state
+			if (zero_crossing) begin //if positive zero_crossing (the signal is high), move to the next state (ACTIVE)
+				next_state_temp = `ACTIVE;
+			end else begin	//else stay in the ARMED state
+				next_state_temp = `ARMED;
+			end
+		`ACTIVE: //when in the ACTIVE state, just wait until we have counted to 255 and then go to the WAIT state
+			if (count_done) begin //if count_done signal is high move to the next state (WAIT)
+				next_state_temp = `WAIT;
+			end else begin // else stay in the active state
+				next_state_temp = `ACTIVE;
+			end
+		`WAIT: 
+			if (wave_display_idle) begin //if wave_display_idle is high we move to the ARMED state 
+				next_state_temp = `ARMED; 
+			end else begin
+				next_state_temp = `WAIT; // else we stay in the wait state
+			end
+		default: next_state_temp = `ARMED;
+		endcase
+end
+
+	//in the active state
+	assign next_counter = (new_sample_ready) ? (counter + 8'b00000001) : counter; //when new_sample_ready signal is high, we increment the counter
+	assign write_address = {~read_index,counter}; //address is the concatenation of the read_index signal and the current count
+	assign count_done =(counter == 8'b11111111); //when the counter has reached 255, the signal count_done goes high
+
+	//determine positive zero crossing
+	assign is_negative = new_sample_in[15] == 1'b1;	//if the module is reset and the sample is less than 0, is_negative signal is high
+	assign zero_crossing = reset ? 1'b0 : (~is_negative & last_is_negative); //if the sample is not negative and the last sample is negative, positive zero crossing
+
+	//state specific assignments
+	assign next_state = next_state_temp; //next_state always holds the value of the temporary next_state register
+	assign write_sample_temp = (state == `ACTIVE) ? new_sample_in[15:8] : 8'b00000000; 
+	assign write_enable = (state  == `ACTIVE);
+	assign write_sample = write_sample_temp + 8'b10000000;
+
+	//invert read_index when wave_display_idle goes high 
+	assign read_index_temp = ~read_index;
+
+endmodule 
